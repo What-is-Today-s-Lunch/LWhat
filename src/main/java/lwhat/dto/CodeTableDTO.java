0371eabac778ad8d9a464fb7abc18aa7d8a5c70b
OngGuny <start1step@naver.com>
2022-10-04 11:33:33 +0900

    손용균 codetableDTO

---
+++ src/main/java/lwhat/dto/CodeTableDTO.java
@@ -0,0 +1,35 @@
+package lwhat.dto;
+
+import java.io.Serializable;
+
+public class CodeTableDTO implements Serializable{
+	public static final long serialVersionUID = 56565656565656L;
+	
+	private String codeValue;
+	private String codeName;
+	
+	public CodeTableDTO() {
+		
+	}// default g
+	
+	public String getCodeValue() {
+		return codeValue;
+	}
+	public void setCodeValue(String codeValue) {
+		this.codeValue = codeValue;
+	}
+	public String getCodeName() {
+		return codeName;
+	}
+	public void setCodeName(String codeName) {
+		this.codeName = codeName;
+	}
+
+	@Override
+	public String toString() {
+		return "CodeTableDTO [codeValue=" + codeValue + ", codeName=" + codeName + "]";
+	}
+	
+	
+	
+}// class 
